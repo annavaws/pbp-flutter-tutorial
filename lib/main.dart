bfa1bfa3cb065fdf14ad9aac01afd02773b364ce
annavaws <annavaws@gmail.com>
2022-11-17 13:28:31 +0700

    lab 8 done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
-import 'package:tutorial_pbp/form.dart';
+import 'package:tutorial_pbp/page/form.dart';
 import 'package:flutter/material.dart';
+import 'package:tutorial_pbp/page/to_do_page.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -81,6 +83,16 @@ class _MyHomePageState extends State<MyHomePage> {
                     MaterialPageRoute(builder: (context) => const MyFormPage()),
                   );
                 },
+              ),
+              ListTile(
+                  title: const Text('To Do'),
+                  onTap: () {
+                      // Route menu ke halaman to do
+                      Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => const ToDoPage()),
+                      );
+                  },
               ),
             ],
           ),
--- lib/page/form.dart
+++ lib/page/form.dart
@@ -1,5 +1,7 @@
 import 'package:tutorial_pbp/main.dart';
 import 'package:flutter/material.dart';
+import '/page/to_do_page.dart';
+
 
 class MyFormPage extends StatefulWidget {
     const MyFormPage({super.key});
@@ -57,6 +59,16 @@ class _MyFormPageState extends State<MyFormPage> {
                           MaterialPageRoute(builder: (context) => const MyFormPage()),
                         );
                       },
+                    ),
+                    ListTile(
+                        title: const Text('To Do'),
+                        onTap: () {
+                            // Route menu ke halaman to do
+                            Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(builder: (context) => const ToDoPage()),
+                            );
+                        },
                     ),
                   ],
                 ),
